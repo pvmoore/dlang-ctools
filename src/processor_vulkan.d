886a354ce1938ea5e3d9b80f84b18a4a5103b930
pvmoore <1926576+pvmoore@users.noreply.github.com>
2023-11-19 16:08:19 +0000

    Update to Vulkan SDK 1.3.268.0

--- src/processor_vulkan.d
+++ src/processor_vulkan.d
@@ -7,7 +7,7 @@ private:
     EConfig config;
     Extractor extractor;
     Emitter emitter;
-    enum vulkanVersion = "1.3.261.1";
+    enum vulkanVersion = "1.3.268.0";
 public:
     void process() {
         prepare();
