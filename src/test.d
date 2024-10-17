28d3f9f661cf15b1e3f0cb6b706b1d1fd6e051c7
pvmoore <1926576+pvmoore@users.noreply.github.com>
2024-10-17 16:14:11 +0100

    Update to Vulkan SDK 1.3.296.0

--- src/test.d
+++ src/test.d
@@ -36,10 +36,10 @@ enum : int {
 
 void main(string[] args) {
 
-    //executeProcessor(D_VULKAN);
+    executeProcessor(D_VULKAN);
     //executeProcessor(J_VULKAN);
     //executeProcessor(J_GLFW);
-    executeProcessor(D_CIMGUI);
+    //executeProcessor(D_CIMGUI);
 
     writefln("Done");
 }
